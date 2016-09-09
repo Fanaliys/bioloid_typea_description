beebb8817862965dcf37947fc8ecac798bc3cc74
Mathieu Rondonneau <mathieu.rondonneau@gmail.com>
2016-09-08 20:31:27 -0700

    Update for kinetic and various cleanup

--- scripts/xacro2urdf.sh
+++ scripts/xacro2urdf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rosrun xacro xacro.py `rospack find bioloid_typea_description`/typea.xacro -o `rospack find bioloid_typea_description`/typea.urdf &&
+rosrun xacro xacro --inorder `rospack find bioloid_typea_description`/typea.xacro -o `rospack find bioloid_typea_description`/typea.urdf &&
 check_urdf `rospack find bioloid_typea_description`/typea.urdf && 
 urdf_to_graphiz `rospack find bioloid_typea_description`/typea.urdf
